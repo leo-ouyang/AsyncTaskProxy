7c614d6166c14e0a9e3b8b4894c5612600dd33be
leo_ouyang <leo_ouyang@163.com>
2016-05-26 13:16:04 +0800

    First submit for library and sample.

---
+++ library/src/main/java/leo/android/core/AsyncTaskProxy.java
@@ -0,0 +1,69 @@
+package leo.android.core;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import leo.android.core.MethodAsyncTask.MethodAsyncTaskListener;
+
+public class AsyncTaskProxy<T> implements MethodAsyncTaskListener {
+	
+	private Map<String, MethodAsyncTask> asyncTaskMap;
+	
+	@SuppressWarnings("unchecked")
+	public final T getProxy() {
+		final Object target = this;
+		return (T) Proxy.newProxyInstance(getClass().getClassLoader(), getClass().getInterfaces(), new InvocationHandler() {
+			@Override
+			public Object invoke(Object object, Method method, Object[] args) throws Throwable {
+				if (args.length == 0 || !(args[args.length-1] instanceof AsyncCallback)) {
+					// No AsyncTask callback
+					return method.invoke(target, args);
+				} else {
+					if (asyncTaskMap == null) {
+						asyncTaskMap = new HashMap<String, MethodAsyncTask>();
+					}
+					AsyncCallback<Object, Object> callback = (AsyncCallback<Object, Object>) args[args.length - 1];
+					MethodAsyncTask asyncTask = new MethodAsyncTask();
+					asyncTask.setTargetMethod(target, method, args);
+					asyncTask.setMethodAsyncTaskListener(AsyncTaskProxy.this);
+					asyncTask.setAsyncCallback(callback);
+					callback.setAsyncTask(asyncTask);
+					String key = method.getName();
+					if (asyncTaskMap.containsKey(key)) {
+						key += "-" + UUID.randomUUID().toString();
+					}
+					asyncTaskMap.put(key, asyncTask);
+					asyncTask.execute();
+					return asyncTask;
+				}
+			}
+		});
+	}
+	
+	// Cancel all AsyncTasks which launched by this class
+	public final void cancelAll() {
+		if (asyncTaskMap != null) {
+			Collection<MethodAsyncTask> values = asyncTaskMap.values();
+			if (values != null) {
+				for (MethodAsyncTask asyncTask : values) {
+					if (asyncTask != null && asyncTask.isRunning()) {
+						asyncTask.cancel(true);
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void onDone(String name) {
+		if (asyncTaskMap != null) {
+			asyncTaskMap.remove(name); // Remove AsyncTask if it finished
+		}
+	}
+
+}
---
+++ sample/src/main/java/leo/android/core/sample/activities/DetailActivity.java
@@ -0,0 +1,85 @@
+package leo.android.core.sample.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import leo.android.core.AsyncCallback;
+import leo.android.core.MethodAsyncTask;
+import leo.android.core.sample.R;
+import leo.android.core.sample.beans.Article;
+import leo.android.core.sample.commons.Consts;
+import leo.android.core.sample.commons.Logger;
+import leo.android.core.sample.dao.IArticleDao;
+import leo.android.core.sample.dao.impl.ArticleDao;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private IArticleDao mArticleDao;
+    private MethodAsyncTask mArticleTextMAT;
+    private Article mArticle;
+    private TextView mTextView;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        mArticle = (Article) intent.getSerializableExtra(Consts.KEY_ARTICLE);
+        if (mArticle == null) {
+            Logger.e("mArticle is null");
+            finish();
+            return ;
+        }
+        getSupportActionBar().setTitle(mArticle.getTitle());
+
+        mTextView = (TextView) findViewById(R.id.text);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mArticleDao = new ArticleDao().getProxy();
+        mArticleTextMAT = mArticleDao.getArticleText(Consts.ARTICLE_BASE_URL + mArticle.getUrl(), callback);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mArticleTextMAT != null && mArticleTextMAT.isRunning()) {
+            mArticleTextMAT.cancel(true);
+        }
+        // or use method instead, this will cancel all AsyncTasks created by mArticleDao.
+//        mArticleDao.cancelAll();
+    }
+
+    private AsyncCallback callback = new AsyncCallback<Integer, String>() {
+        @Override
+        protected void onResult(int err, String... texts) {
+            Logger.d("begin, err = " + err);
+            if (err == Consts.ERRCODE_FAILED) {
+                Toast.makeText(DetailActivity.this, R.string.failed_to_load_data, Toast.LENGTH_LONG).show();
+            } else if (err == Consts.ERRCODE_NET_BUGEILI) {
+                Toast.makeText(DetailActivity.this, R.string.network_bugeili, Toast.LENGTH_LONG).show();
+            } else if (err == Consts.ERRCODE_SUCCESSED) {
+                String text = texts[0];
+                if (text != null) {
+                    mTextView.setText(Html.fromHtml(text));
+                }
+            }
+            mProgressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected void onCancel() {
+            Toast.makeText(DetailActivity.this, R.string.cancelled_load_article, Toast.LENGTH_LONG).show();
+        }
+    };
+}
